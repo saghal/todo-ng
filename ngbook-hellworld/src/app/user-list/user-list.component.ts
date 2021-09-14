fafe7f9c39092021f4324833cfd1949421b435b3
saghal <saghalov@gmail.com>
2021-09-14 14:30:03 +0430

    ngbook: add names in template

--- ngbook-hellworld/src/app/user-list/user-list.component.ts
+++ ngbook-hellworld/src/app/user-list/user-list.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  names : string[];
+  constructor() { 
+    this.names = ["saghal", "saqal"]
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
