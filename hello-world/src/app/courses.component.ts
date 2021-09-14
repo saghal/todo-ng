fb2da295f14a928637023d2da29d6ddca5769d08
saghal <saghalov@gmail.com>
2021-09-14 11:45:21 +0430

    mosh: event binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: '<h2>ok courses</h2>',
+  template: `
+    <h2>ok courses</h2>
+    <button (click)="onFire()" [class.btn-primary]="isActive">
+      click this!
+    </button>
+    <input [(ngModel)]="email" (keyup.enter)="onInput()" />
+  `,
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  isActive = true;
+  email = 'a@a.com';
+  onFire() {
+    console.log('ok this is clicked');
+  }
+  onInput() {
+    console.log(this.email);
+  }
+}
