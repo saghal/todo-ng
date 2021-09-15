91f1d6127eadc5d875c84885a19dfbf99ed707e9
saghal <saghalov@gmail.com>
2021-09-15 12:38:18 +0430

    ngbook.reddit: add article model

--- reddit/src/app/article/article.component.ts
+++ reddit/src/app/article/article.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  //@HostBinding ("attr.class") cssClass = "row";
+  article : Article;
 
-  constructor() { }
+  constructor() {
+    this.article = new Article("google","https://www.google.com",15);
+     }
+   voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+    }
 
+   voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+    }
   ngOnInit(): void {
   }
 
