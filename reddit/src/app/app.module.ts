8da14dbea992015c777c0f2ea2f714f1d3c3447a
saghal <saghalov@gmail.com>
2021-09-15 10:34:58 +0430

    ngbook.reddit: add article component

--- reddit/src/app/app.module.ts
+++ reddit/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
