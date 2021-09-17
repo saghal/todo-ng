afe1b2972b788a3c7ed585d184ee1f50a409c63f
saghal <saghalov@gmail.com>
2021-09-17 20:57:04 +0430

    ngbook.inventory: add productRow component

--- inventory/src/app/product-list/product-list.component.ts
+++ inventory/src/app/product-list/product-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../product.model';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() productList: Product[];
+  @Output() onProductSelected: EventEmitter<Product>;
+  private currentProduct: Product;
+  constructor() {
+    this.onProductSelected = new EventEmitter();
   }
-
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+  ngOnInit(): void {}
 }
