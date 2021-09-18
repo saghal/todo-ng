7df1b0efdac64f9324d15365bb8258eb3d13e52a
saghal <saghalov@gmail.com>
2021-09-18 15:13:58 +0430

    ngbook: add reactive form

--- template-driven-form/src/app/app.module.ts
+++ template-driven-form/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroFormComponent],
-  imports: [BrowserModule, CommonModule, FormsModule],
+  declarations: [AppComponent, HeroFormComponent, ReactiveFormComponent],
+  imports: [BrowserModule, CommonModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
