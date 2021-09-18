a5d1fbd5bb08f9521df6ee0832dcaf17db8b009e
saghal <saghalov@gmail.com>
2021-09-18 11:43:58 +0430

    mosh: add favorite component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent],
+  declarations: [AppComponent, CoursesComponent, FavoriteComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
