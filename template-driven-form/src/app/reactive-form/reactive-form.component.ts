1307fff3297b35c53c152b3e3528c851e872944b
saghal <saghalov@gmail.com>
2021-09-18 16:22:23 +0430

    ngbook: we have error in reactive forms Type 'AbstractControl' is missing the following properties from type 'FormControl': registerOnChange, registerOnDisabledChange, _applyFormState

--- template-driven-form/src/app/reactive-form/reactive-form.component.ts
+++ template-driven-form/src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  myForm: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      sku: ['abc123'],
+    });
+  }
+  onSubmit(form: string): void {
+    console.log('here, ', form);
   }
 
+  ngOnInit(): void {}
 }
