171b52f98cbce6712f109c16f545ddebcb8031ab
saghal <saghalov@gmail.com>
2021-09-19 19:25:22 +0430

    mosh.helloworld: add like component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, FavoriteComponent, TitleCasePipe, PanelComponent],
+  declarations: [AppComponent, CoursesComponent, FavoriteComponent, TitleCasePipe, PanelComponent, LikeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
