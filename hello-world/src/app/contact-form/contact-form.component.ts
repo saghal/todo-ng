4bb7e6365c95b280f38db45919e715602e068a9b
saghal <saghalov@gmail.com>
2021-09-20 09:22:52 +0430

    mosh: add field required validation to template driven form

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  log(firstName: any): void {
+    console.log(firstName);
   }
-
 }
