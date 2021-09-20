91a20bd427adab99ae402f9d1e5899d983f9b732
saghal <saghalov@gmail.com>
2021-09-20 14:35:24 +0430

    mosh: implement arrayForm in adding and removing manner

--- hello-world/src/app/new-form-component/new-form-component.component.ts
+++ hello-world/src/app/new-form-component/new-form-component.component.ts
@@ -1,15 +1,32 @@
+import {
+  FormArray,
+  FormGroup,
+  FormControl,
+  AbstractControl,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-form-component',
   templateUrl: './new-form-component.component.html',
-  styleUrls: ['./new-form-component.component.css']
+  styleUrls: ['./new-form-component.component.css'],
 })
 export class NewFormComponentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form = new FormGroup({
+    topics: new FormArray([]),
+  });
+  constructor() {}
+  addTopic(topic: HTMLInputElement): void {
+    (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
+    //above line can alter with this.topic ---> come from get topics()
+  }
+  removeTopic(topic: AbstractControl): void {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+  get topics() {
+    return this.form.get('topics') as FormArray;
   }
 
+  ngOnInit(): void {}
 }
