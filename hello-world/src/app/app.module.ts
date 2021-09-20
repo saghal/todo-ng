ab3a31d81f35ad470262f553742fe6b5749c47da
saghal <saghalov@gmail.com>
2021-09-20 11:42:34 +0430

    mosh: create first reactive form

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
@@ -13,8 +13,17 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, FavoriteComponent, TitleCasePipe, PanelComponent, LikeComponent, ContactFormComponent, SignupFormComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    FavoriteComponent,
+    TitleCasePipe,
+    PanelComponent,
+    LikeComponent,
+    ContactFormComponent,
+    SignupFormComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+  styleUrls: ['./signup-form.component.css'],
 })
 export class SignupFormComponent implements OnInit {
+  form = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
