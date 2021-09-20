22dec7419f14d82bec0d7cf5b252fca0aebc8fec
saghal <saghalov@gmail.com>
2021-09-20 15:43:48 +0430

    mosh: implement login reactive form with validators(except new and confirm new is equal and old password checking with async operation)

--- hello-world/src/app/change-password-form/change-password-form.component.ts
+++ hello-world/src/app/change-password-form/change-password-form.component.ts
@@ -1,15 +1,27 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-change-password-form',
   templateUrl: './change-password-form.component.html',
-  styleUrls: ['./change-password-form.component.css']
+  styleUrls: ['./change-password-form.component.css'],
 })
 export class ChangePasswordFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form = new FormGroup({
+    oldPassword: new FormControl('', Validators.required),
+    newPassword: new FormControl('', Validators.required),
+    confirmNewPassword: new FormControl('', Validators.required),
+  });
+  constructor() {}
+  get oldPassword(): any {
+    return this.form.get('oldPassword');
+  }
+  get newPassword(): any {
+    return this.form.get('newPassword');
+  }
+  get confirmNewPassword(): any {
+    return this.form.get('confirmNewPassword');
   }
 
+  ngOnInit(): void {}
 }
