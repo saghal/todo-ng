bcb68413eea5a9fa3351a6f749c4089b22b780b3
saghal <saghalov@gmail.com>
2021-09-20 12:04:13 +0430

    mosh: adding validation to reactive forms

--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
@@ -7,10 +7,20 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class SignupFormComponent implements OnInit {
   form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl(),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    password: new FormControl('', Validators.required),
   });
   constructor() {}
 
   ngOnInit(): void {}
+
+  get username(): any {
+    return this.form.get('username');
+  }
+  get password(): any {
+    return this.form.get('password');
+  }
 }
