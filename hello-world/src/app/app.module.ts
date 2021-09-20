0b3e2dc36328be1a21808a000d1117a8092b34b4
saghal <saghalov@gmail.com>
2021-09-20 08:47:27 +0430

    mosh: add contact-form component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, FavoriteComponent, TitleCasePipe, PanelComponent, LikeComponent],
+  declarations: [AppComponent, CoursesComponent, FavoriteComponent, TitleCasePipe, PanelComponent, LikeComponent, ContactFormComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
