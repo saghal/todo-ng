a1ab78b89eed7451dc5e921d8d60e102dca230d4
saghal <saghalov@gmail.com>
2021-09-22 11:46:41 +0330

    maxim: inject services into services

--- Complete course/services-start/src/app/app.component.ts
+++ Complete course/services-start/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from "@angular/core";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [AccountsService],
+  // providers: [AccountsService],
 })
 export class AppComponent implements OnInit {
   accounts: { name: string; status: string }[] = [];
--- Complete course/services-start/src/app/new-account/new-account.component.ts
+++ Complete course/services-start/src/app/new-account/new-account.component.ts
@@ -5,7 +5,7 @@ import { LoggingService } from "../logging.service";
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [LoggingService],
+  // providers: [LoggingService],
 })
 export class NewAccountComponent {
   constructor(
@@ -15,6 +15,5 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAcount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
