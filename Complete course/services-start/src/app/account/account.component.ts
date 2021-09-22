193651826d794306f76b57460b6d78c69ffa9066
saghal <saghalov@gmail.com>
2021-09-22 11:20:19 +0330

    maxim: remove instance of services in child component and just keep parent account service (for hierarchy manner)

--- Complete course/services-start/src/app/account/account.component.ts
+++ Complete course/services-start/src/app/account/account.component.ts
@@ -5,7 +5,7 @@ import { LoggingService } from "../logging.service";
   selector: "app-account",
   templateUrl: "./account.component.html",
   styleUrls: ["./account.component.css"],
-  providers: [LoggingService, AccountsService],
+  providers: [LoggingService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
