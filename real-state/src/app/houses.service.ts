ecf003d8e2e181dc3a1ed02ae4c034a2af9e7dae
saghal <saghalov@gmail.com>
2021-09-25 12:49:27 +0330

    realstate: add house service

---
+++ real-state/src/app/houses.service.ts
@@ -0,0 +1,22 @@
+export class HousesService {
+  houses = [
+    {
+      houseId: 123,
+      style: 'Apartment',
+      size: 200,
+      yearBuilt: 2008,
+      type: 'sale',
+      address: 'tehran',
+      updated: '2020-1-1',
+    },
+    {
+      houseId: 1234,
+      style: 'Apartment',
+      size: 200,
+      yearBuilt: 2009,
+      type: 'sale',
+      address: 'tehran',
+      updated: '2020-2-1',
+    },
+  ];
+}
