69a58b1230d86c7dad47e92c3f545b21940b8376
saghal <saghalov@gmail.com>
2021-09-25 14:19:46 +0330

    mosh: get data with http

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+interface postStructure {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
-  posts: any;
+  posts: postStructure[];
   constructor(private http: HttpClient) {
     this.http
-      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .get<postStructure[]>('https://jsonplaceholder.typicode.com/posts/')
       .subscribe((response) => {
         this.posts = response;
       });
