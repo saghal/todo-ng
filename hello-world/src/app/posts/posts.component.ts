13ead0818fd4f8b17af408e94fd46738468c1608
saghal <saghalov@gmail.com>
2021-09-25 14:45:11 +0330

    mosh: http post for create new post

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -13,16 +13,25 @@ interface postStructure {
 })
 export class PostsComponent implements OnInit {
   posts: postStructure[];
+  private url: string = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: HttpClient) {
-    this.http
-      .get<postStructure[]>('https://jsonplaceholder.typicode.com/posts/')
-      .subscribe((response) => {
-        this.posts = response;
-      });
+    this.http.get<postStructure[]>(this.url).subscribe((response) => {
+      this.posts = response;
+    });
   }
 
   onClick(post: object): void {
     console.log(post);
+  }
+  createPost(title: HTMLInputElement): void {
+    this.http
+      .post<postStructure>(this.url, {
+        title: title.value,
+      })
+      .subscribe((response) => {
+        this.posts.splice(0, 0, response);
+      });
+    console.log('in create post');
   }
   ngOnInit(): void {}
 }
