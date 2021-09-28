f93e5d46fb5b432f3601fbcce1dfed96b387bb27
saghal <saghalov@gmail.com>
2021-09-28 13:52:28 +0330

    realstate: add routing to house component

--- real-state/src/app/app-routing.module.ts
+++ real-state/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HouseComponent } from './houses/house/house.component';
 import { HousesComponent } from './houses/houses.component';
 import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HousesComponent },
+  { path: 'houses/:id', component: HouseComponent },
 ];
 
 @NgModule({
