45b2e1cd5dedc18b709d5ccfa9e4e41fecd165d4
saghal <saghalov@gmail.com>
2021-09-29 10:49:15 +0330

    realstate: add Upadte house component and functions

--- real-state/src/app/houses/edit-house/edit-house.component.ts
+++ real-state/src/app/houses/edit-house/edit-house.component.ts
@@ -1,15 +1,39 @@
+import { houseStructure } from './../../common/interfaces';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HousesService } from 'src/app/services/houses.service';
 
 @Component({
   selector: 'app-edit-house',
   templateUrl: './edit-house.component.html',
-  styleUrls: ['./edit-house.component.css']
+  styleUrls: ['./edit-house.component.css'],
 })
 export class EditHouseComponent implements OnInit {
-
-  constructor() { }
+  house: houseStructure | undefined;
+  index: number;
+  constructor(
+    private route: ActivatedRoute,
+    private housesService: HousesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    let temp = this.route.snapshot.paramMap.get('id');
+    let idNumber: number;
+    if (typeof temp != undefined && temp) {
+      idNumber = +temp;
+    }
+    // an else for route to 404 page same in house component
+    this.housesService.houses.find((house) => {
+      if (house.id === idNumber) {
+        this.house = house;
+        this.index = this.housesService.houses.indexOf(this.house);
+      }
+    });
   }
 
+  updateHouse(form: houseStructure): void {
+    this.housesService.houseUpdate(form, this.index);
+    this.router.navigate(['/houses', this.house?.id]);
+  }
 }
--- real-state/src/app/services/houses.service.ts
+++ real-state/src/app/services/houses.service.ts
@@ -1,6 +1,9 @@
-import { houseStructure } from '../common/interfaces';
-
+import { Injectable } from '@angular/core';
+import { houseStructure } from './../common/interfaces';
+import { DateService } from './date.service';
+@Injectable()
 export class HousesService {
+  constructor(private dateService: DateService) {}
   houses: houseStructure[] = [
     {
       id: 123,
@@ -34,5 +37,18 @@ export class HousesService {
       address: form.address,
       updated: '1200-0-0',
     });
+  }
+
+  houseUpdate(house: houseStructure, index: number): void {
+    const currentDate = this.dateService.getCurrentDate();
+    this.houses[index].style = house.style;
+    this.houses[index].size = house.size;
+    this.houses[index].yearBuilt = house.yearBuilt;
+    this.houses[index].type = house.type;
+    this.houses[index].status = house.status;
+    this.houses[index].address = house.address;
+    this.houses[index].updated = currentDate;
+    this.houses[index].id = house.id;
+    console.log('houses be mola', this.houses[index]);
   }
 }
