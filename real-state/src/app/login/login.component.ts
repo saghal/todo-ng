b3ddc75832dbf521966449c7bdda38ed7886f01b
saghal <saghalov@gmail.com>
2021-09-29 16:53:11 +0330

    realstate: save users in local storage

--- real-state/src/app/login/login.component.ts
+++ real-state/src/app/login/login.component.ts
@@ -11,7 +11,36 @@ export class LoginComponent implements OnInit {
   isLogged: boolean;
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let JSONDatas = [
+      { id: 1, username: 'saghal', password: 'saghal', admin: true },
+      { id: 2, username: 'jes', password: 'jes', admin: false },
+      { id: 3, username: 'walter', password: 'walter', admin: false },
+    ];
+
+    localStorage.setItem('datas', JSON.stringify(JSONDatas));
+    localStorage.setItem(
+      'saghal',
+      JSON.stringify({ id: 1, password: 'saghal', admin: true })
+    );
+    localStorage.setItem(
+      'jes',
+      JSON.stringify({ id: 2, password: 'jes', admin: false })
+    );
+    localStorage.setItem(
+      'walter',
+      JSON.stringify({ id: 3, password: 'walter', admin: false })
+    );
+
+    let datas = localStorage.getItem('datas');
+    let account: string | null = localStorage.getItem('saghal');
+    if (account !== null) {
+      let saghalAccount = JSON.parse(account);
+      console.log('password:', saghalAccount.password);
+      console.log(saghalAccount);
+    }
+    console.log(datas, datas?.length);
+  }
   onSubmit(form: { username: string; password: string }): void {
     this.isLogged = this.authService.login(form.username, form.password);
     if (this.isLogged) {
