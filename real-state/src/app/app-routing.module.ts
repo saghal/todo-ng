0490c9df9f7cac3888887fb8770fb8b80cc4f821
saghal <saghalov@gmail.com>
2021-09-29 09:20:45 +0330

    realstate: add routing to edit page

--- real-state/src/app/app-routing.module.ts
+++ real-state/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditHouseComponent } from './houses/edit-house/edit-house.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HouseComponent } from './houses/house/house.component';
@@ -8,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HousesComponent },
+  { path: 'houses/edit/:id', component: EditHouseComponent },
   { path: 'houses/new', component: NewHouseComponent },
   { path: 'houses/:id', component: HouseComponent },
 ];
